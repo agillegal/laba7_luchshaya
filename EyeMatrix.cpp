8ecdc90893049b436ddbe92f0b792410b228a4d1
madassG <focusssspro@yandex.ru>
2022-06-17 22:59:06 +0300

    deleted all sufficient files

--- EyeMatrix.cpp
+++
@@ -1,6 +0,0 @@
-#include "EyeMatrix.h"
-EyeMatrix::EyeMatrix(int N):Matrix(N,N) {
-    for(int i= 0; i<N; i++) {
-        set(1, i, i);
-    }
-}
--- SymMatrix.cpp
+++
@@ -1,8 +0,0 @@
-#include "SymMatrix.h"
-SymMatrix::SymMatrix(int N): Matrix(N, N) {};
-void SymMatrix::random_fill(double min, double max) {
-    Matrix temp(_rows, _rows);
-    temp.random_fill(0, 1);
-    temp= (temp*temp.T()*(max-min))/_rows+min;
-    copy(temp);
-}
